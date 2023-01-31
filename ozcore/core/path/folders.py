b502c496bdcb293f13ac31288187d43e7e5cab91
OzInClouds <ozgurkalan@gmail.com>
2023-01-31 06:54:11 +0000

    refactored folders module and refined tests
    * added listdir method
    * applied check_path method to other methods for type checking and file/folder checking
    * added Documents path property
    * fixed OneDrive path property
    * tests refactored and refined

--- ozcore/core/path/folders.py
+++ ozcore/core/path/folders.py
@@ -1,14 +1,14 @@
-"""  
-Useful paths and folders, search in folders and temporary folder helper methods.
+""" 
+Common paths and folders, search in folders helper methods.
 
 warning:
-    Folder paths are for **OSX** only. 
+    Folder paths are returned as POSIXPATH for **OSX** and WINDOWSPATH for win32 systems. 
     
 note:
     Special folders like Google Drive and OneDrive may not be available in your directories. Or your directory structure may be different. 
 
 """
-from pathlib import Path, PosixPath
+from pathlib import Path, PosixPath, WindowsPath
 from typing import Union
 from typeguard import check_argument_types
 
@@ -17,6 +17,7 @@ class Folder:
     """
     Essential folder paths as posixpath:
         - Downloads
+        - Documents
         - gDrive
         - iCloud
         - OneDrive
@@ -31,21 +32,30 @@ class Folder:
     Example, Search Downloads folder::
 
         from ozcore import core
-        core.folder.search(path=core.folder.Downloads, file="the_file_searched.xl**")
+        core.path.search(path=core.path.Downloads, file="the_file_searched.xl**")
+
+    Example, List directory::
+
+        from ozcore import core
+        core.path.listdir(path=core.path.Downloads)
 
     Example, is_a_subfolder::
 
         from ozcore import core
-        core.folder.is_a_subfolder(path_to_check=Path("."), parent_folder=core.BASE_DIR)
+        core.path.is_a_subfolder(path_to_check=Path("."), parent_folder=core.BASE_DIR)
+
+    Example, check_path::
+
+        from ozcore import core
+        core.path.check_path(path=core.path.Downloads.joinpath('test.xlsx'), file=True)
     """
 
     @property
-    def Downloads(self):
-        """
-        MacOS Downloads folder path
+    def Downloads(self) -> Union[PosixPath, WindowsPath]:
+        """Downloads folder path
 
         returns:
-            POSIXPATH to Downloads folder
+            POSIXPATH or WindowsPath to Downloads folder
         """
         path = Path().home().joinpath("Downloads")
         if path.exists():
@@ -54,12 +64,24 @@ class Folder:
             raise FileNotFoundError("Downloads folder not found!")
 
     @property
-    def gDrive(self):
-        """
-        Google Drive folder path
+    def Documents(self) -> Union[PosixPath, WindowsPath]:
+        """Documents folder path
 
         returns:
-            POSIXPATH to gDrive
+            POSIXPATH or WindowsPath to Documents folder
+        """
+        path = Path().home().joinpath("Documents")
+        if path.exists():
+            return path
+        else:
+            raise FileNotFoundError("Documents folder not found!")
+
+    @property
+    def gDrive(self) -> Union[PosixPath, WindowsPath]:
+        """Google Drive folder path
+
+        returns:
+            POSIXPATH or WindowsPath to gDrive
         """
         ls = [
             e
@@ -77,12 +99,11 @@ class Folder:
             raise FileNotFoundError("Google Drive folder not found!")
 
     @property
-    def iCloud(self):
-        """
-        iCloud folder path
+    def iCloud(self) -> Union[PosixPath, WindowsPath]:
+        """iCloud folder path
 
         returns:
-            POSIXPATH to iCloud
+            POSIXPATH or WindowsPath to iCloud
         """
         ls = sorted(
             [
@@ -102,12 +123,11 @@ class Folder:
             raise FileNotFoundError("iCloud folder not found!")
 
     @property
-    def OneDrive(self):
-        """
-        OneDrive folder path
+    def OneDrive(self) -> Union[PosixPath, WindowsPath]:
+        """OneDrive folder path
 
         returns:
-            POSIXPATH to OneDrive
+            POSIXPATH or WindowsPath to OneDrive
         """
         ls = [
             e
@@ -117,6 +137,9 @@ class Folder:
 
         if len(ls) == 1:
             ls = ls[0]
+        elif len(ls)>1:
+            ls = ls[1] 
+            # if there is more than one OneDrive probably the second one is the Business version you're looking for
 
         path = Path(ls).resolve()
         if path.exists():
@@ -124,35 +147,52 @@ class Folder:
         else:
             raise FileNotFoundError("OneDrive folder not found!")
 
-    def search(self, path, file):
-        """
-        search files in a given folder path
+    def listdir(
+        self, path: Union[str, PosixPath, WindowsPath]
+    ) -> list[Union[None, PosixPath, WindowsPath]]:
+        """ls files in a folder
 
         parameters:
             path: a str or posixpath path
-            file: str, file to search; searches as glob, e.g. **"**/.json"**
 
         returns:
             list, files in POSIXPATH type
+        """
+        path = self.check_path(path, is_file=False, get_parent=True).resolve()
+
+        return [e for e in path.iterdir()]
+
+    def search(
+        self, path: Union[str, PosixPath, WindowsPath], file: str
+    ) -> list[Union[None, PosixPath, WindowsPath]]:
+        """Search files in a given folder path
+
+        parameters:
+            path: a str or posixpath or WindowsPath path
+            file: str, file to search; searches as glob, e.g. **"**/.json"**
+
+        returns:
+            list, files in POSIXPATH or WindowsPath type
 
         usage::
 
             from ozcore import core
-            core.folder.search(core.folder.gDrive.joinpath("folder name"), "*.xlsx")
+            core.path.search(core.path.gDrive.joinpath("folder name"), "*.xlsx")
         """
+        check_argument_types()
 
-        path = Path(path).resolve()
-
-        if not path.exists() or not path.is_dir():
-            raise FileNotFoundError(f"{path} is not available!")
-
+        path = self.check_path(path, is_file=False, get_parent=True).resolve()
         ls = path.rglob(file)
         result = sorted([e for e in ls], reverse=False)
+
         return result
 
-    def is_a_subfolder(self, path_to_check: PosixPath, parent_folder: PosixPath):
-        """
-        check if a given path is in parent folder
+    def is_a_subfolder(
+        self,
+        path_to_check: Union[str, PosixPath, WindowsPath],
+        parent_folder: Union[str, PosixPath, WindowsPath],
+    ) -> bool:
+        """Check if a given path is in parent folder
 
         parameters:
             path_to_check: posixpath, a path to match with base folder
@@ -161,8 +201,10 @@ class Folder:
         return:
             boolean
         """
-        path = path_to_check.resolve()
-        base = parent_folder.resolve()
+        check_argument_types()
+
+        path = self.check_path(path_to_check, is_file=False, get_parent=True).resolve()
+        base = self.check_path(parent_folder, is_file=False, get_parent=True).resolve()
 
         if path == base:
             return True
@@ -173,10 +215,12 @@ class Folder:
             return False
 
     def check_path(
-        self, path: Union[str, PosixPath], is_file: bool = False
-    ) -> PosixPath:
-        """
-        Checks the given ``path`` param, converts it to an absolute path
+        self,
+        path: Union[str, PosixPath, WindowsPath],
+        is_file: bool = False,
+        get_parent: bool = False,
+    ) -> Union[PosixPath, WindowsPath]:
+        """Checks the given ``path`` parameter, converts it to an absolute path.
 
         warning:
             All relative paths are ignored and Exception will be raised.
@@ -185,28 +229,30 @@ class Folder:
             This method checks the type of the arguments and raise Exception if not satisfied.
 
         parameters:
-            path: str|PosixPath, can be Pathlike path or a relative path
+            path: str|PosixPath|WindowsPath, can be Pathlike path or a relative path
             is_file: bool, default False, False: a directory, True: a file
+            get_parent: bool, default False, True: get file's parent (is_file should be False)
 
         returns:
-            Absolute PosixPath; checks if path exists.
+            Absolute PosixPath or WindowsPath;
+            Raise error if directory or file not exists.
         """
         # check type of the given path parameter
         check_argument_types()
 
         if isinstance(path, str):
-            path = Path(path)
+            path = Path(path).absolute()
 
-        if not path.is_absolute():
-            raise Exception("Only absoulte paths are allowed.")
-
-        elif not path.exists():
+        if not path.exists():
             raise Exception(f"{str(path)} does not exists")
 
-        elif is_file == False and not path.is_dir():
-            raise TypeError(f"{str(path)} is not a directory")
+        if is_file == False and not path.is_dir():
+            if get_parent == True:
+                path = path.parent
+            else:
+                raise TypeError(f"{str(path)} is not a directory")
 
-        elif is_file == True and not path.is_file():
+        if is_file == True and not path.is_file():
             raise TypeError(f"{str(path)} is not a file")
 
         return path
