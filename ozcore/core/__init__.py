5d8aadd39927e41d8a994994b89458d940692a50
OzInClouds <ozgurkalan@gmail.com>
2023-02-03 13:17:24 +0300

    isorted and blacked imports

--- ozcore/core/__init__.py
+++ ozcore/core/__init__.py
@@ -7,25 +7,24 @@ note:
 
 """
 
+# # df and utils
+from ozcore.core import df, utils
 # path module::Folder
 from ozcore.core.path.folders import Folder as __Folder
-folder = __Folder()  
+
+folder = __Folder()
 
 # # SQL
 from ozcore.core.data.sqlite.sqlite import Sqlite as __sqlite
+
 sql = __sqlite()
 
 # # CSV
 from ozcore.core.data.csv.base import Base as __csv_Base
+
 csv = __csv_Base()
 
 # # aggrid module
 from ozcore.core.aggrid.aggrid import Grid as __agGrid
+
 view = __agGrid().view
-
-# # utils
-from ozcore.core import utils
-
-# # df
-from ozcore.core import df
-
